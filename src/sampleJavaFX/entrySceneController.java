bf90f4660dec5b199be6e53c202dee2e1dabccf4
Soheeb Amin <soheeb@gmail.com>
2020-10-23 16:29:51 -0400

    now two scenes are linked

--- src/sampleJavaFX/entrySceneController.java
+++ src/sampleJavaFX/entrySceneController.java
@@ -1,10 +1,17 @@
 package sampleJavaFX;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.CheckBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -30,6 +37,18 @@ public class entrySceneController implements Initializable{
             checked += "\nThird";
 
         this.theLabel.setText(checked);
+    }
+
+    /* The all important code that actually allows us to change to a different scene! */
+
+    public void changeToSecondScene(ActionEvent event) throws IOException {
+        Parent secondSceneParent = FXMLLoader.load(getClass().getResource(("secondScene.fxml")));
+        Scene secondSceneScene = new Scene(secondSceneParent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();   // need this to get the stage info
+
+        window.setScene(secondSceneScene);
+        window.show();
 
     }
 
