14af641b5f70ed9261ea25a90703c4d8b5563446
Soheeb Amin <soheeb@gmail.com>
2020-10-22 21:10:40 -0400

    first logic set (checkboxes)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,10 +2,39 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.CheckBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable{
+
+    @FXML private Label theLabel;
+    @FXML private CheckBox firstbox;
+    @FXML private CheckBox secondbox;
+    @FXML private CheckBox thirdbox;
+
+    public void runClicked(){
+        String checked = "The checkboxes selected are:";
+
+        if (firstbox.isSelected())
+            checked += "\nFirst";
+        if (secondbox.isSelected())
+            checked += "\nSecond";
+        if (thirdbox.isSelected())
+            checked += "\nThird";
+
+        this.theLabel.setText(checked);
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        theLabel.setText(""); //empty, because it the Controller will turn it into text based on what is checked.
+    }
 }
+
+
+
